f231e0f85540974d88e26002fca762ee662aa371
chakphet_ph <chakphet_ph@kkumail.com>
2022-06-10 10:03:27 +0700

    feat: get data by call api

--- lib/screens/beer_list.dart
+++ lib/screens/beer_list.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter_code_challenge/constants/texts.dart';
+import 'package:flutter_code_challenge/models/beer/beer_model.dart';
 
 class BeerList extends StatefulWidget {
   const BeerList({Key? key}) : super(key: key);
@@ -9,6 +13,16 @@ class BeerList extends StatefulWidget {
 }
 
 class _BeerListState extends State<BeerList> {
+  Future<List<BeerModel>> getBeers() async {
+    const url = 'https://api.punkapi.com/v2/beers';
+    final response = await http.get(Uri.parse(url));
+
+    List<dynamic> data = jsonDecode(response.body);
+    List<BeerModel> beers =
+        data.map((data) => BeerModel.fromJson(data)).toList();
+    return beers;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,11 +30,26 @@ class _BeerListState extends State<BeerList> {
       appBar: AppBar(
         title: const Text(appTitle),
       ),
-      body: Center(
-        child: Text(
-          'Beer List',
-          style: Theme.of(context).textTheme.displayLarge,
-        ),
+      body: FutureBuilder(
+        builder: (context, AsyncSnapshot<List<BeerModel>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return Text('😔 ${snapshot.error}');
+          } else if (snapshot.hasData) {
+            final data = snapshot.data;
+            return ListView.builder(
+              itemCount: data!.length,
+              itemBuilder: (context, index) {
+                BeerModel beer = snapshot.data![index];
+                return Text(beer.name);
+              },
+            );
+          } else {
+            return const Text('NONE');
+          }
+        },
+        future: getBeers(),
       ),
     );
   }
